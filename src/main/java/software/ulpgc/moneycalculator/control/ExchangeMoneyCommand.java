07d5bc05882224e52e94ff11361cdb9476ad0cf6
PedroRS9 <pedroglv98@gmail.com>
2024-01-18 10:40:03 +0000

    ExchangeMoneyCommand finished

---
+++ src/main/java/software/ulpgc/moneycalculator/control/ExchangeMoneyCommand.java
@@ -0,0 +1,36 @@
+package software.ulpgc.moneycalculator.control;
+
+import software.ulpgc.moneycalculator.CurrencyDialog;
+import software.ulpgc.moneycalculator.ExchangeRateLoader;
+import software.ulpgc.moneycalculator.MoneyDialog;
+import software.ulpgc.moneycalculator.MoneyDisplay;
+import software.ulpgc.moneycalculator.fixerapi.FixerExchangeLoader;
+import software.ulpgc.moneycalculator.model.Currency;
+import software.ulpgc.moneycalculator.model.ExchangeRate;
+import software.ulpgc.moneycalculator.model.Money;
+
+import java.util.List;
+
+public class ExchangeMoneyCommand implements Command{
+    private final MoneyDialog moneyDialog;
+    private final CurrencyDialog currencyDialog;
+    private final ExchangeRateLoader exchangeRateLoader;
+    private final MoneyDisplay moneyDisplay;
+
+    public ExchangeMoneyCommand(MoneyDialog moneyDialog, CurrencyDialog currencyDialog, ExchangeRateLoader exchangeRateLoader, MoneyDisplay moneyDisplay) {
+        this.moneyDialog = moneyDialog;
+        this.currencyDialog = currencyDialog;
+        this.exchangeRateLoader = exchangeRateLoader;
+        this.moneyDisplay = moneyDisplay;
+    }
+
+    @Override
+    public void execute() {
+        Money money = moneyDialog.get();
+        Currency currency = currencyDialog.get();
+        ExchangeRate exchangeRate = exchangeRateLoader.load(money.currency(), currency);
+        Money result = new Money(money.amount() * exchangeRate.rate(), currency);
+        moneyDisplay.show(result);
+
+    }
+}
